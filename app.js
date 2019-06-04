b0e28dcc7fd133c82772783f028a42eaf9d168ff
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2019-06-04 01:36:16 -0700

    cc

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const cluster = require('cluster');
+const os = require('os');
+
 
 const config = require('./config');
 
@@ -5,11 +8,42 @@ const server = require('./lib/server');
 const routines = require('./lib/routines');
 
 const app = {
-    init: function (config = {}){
-        server.init(config)
-            .then(routines.init);
+    init: function (config = {}) {
+        return new Promise(function (resolve, reject) {
+            if(config.cluster){
+                app.initCluster(config)
+                    .then(resolve);
+            }else {
+                server.init(config)
+                    .then(routines.init)
+                    .then(resolve);
+            }
+        })
 
     },
+    initCluster: function (config) {
+        return new Promise(function (resolve, reject) {
+            if (cluster.isMaster) {
+                routines.init(config)
+                    .then(app.initFork)
+                    .then(function () {
+                        console.log('Master is Running...')
+                    });
+            } else {
+                server.init(config);
+            }
+            resolve(config);
+        })
+    },
+    initFork: function (config) {
+        return new Promise(function (resolve, reject) {
+            let maxCore = config.maxCluster || os.cpus().length;
+            for(var i=0; i<maxCore; i++){
+                cluster.fork();
+            }
+            resolve(config);
+        })
+    }
 };
 
 app.init(config);
--- config.js
+++ config.js
@@ -1,17 +1,23 @@
 const ENV = {
     dev:{
         envName: 'dev',
+        hostname: 'localhost',
         httpPort: 3000,
         httpsPort: 3001,
         httpsKey: '',
         httpsCert: '',
+        cluster: true,
+        maxCluster: 0, // 0 for Max
     },
     production:{
         envName: 'production',
+        hostname: 'localhost',
         httpPort: 5000,
         httpsPort: 5001,
         httpsKey: '',
         httpsCert: '',
+        cluster: true,
+        maxCluster: 0, // 0 for Max
     }
 };
 
